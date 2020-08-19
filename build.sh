0b92d51d15ae246c34f00f82c233da8fdbc386b3
Carmine Zaccagnino <carmine@carminezacc.com>
2020-08-19 19:30:48 +0200

    cambiamenti build
    Signed-off-by: Carmine Zaccagnino <carmine@carminezacc.com>

--- build.sh
+++ build.sh
@@ -4,9 +4,7 @@ git clone https://github.com/flutter/flutter.git -b beta
 
 export FLUTTER_HOME=./flutter
 export FLUTTER=./flutter/bin/flutter
-$FLUTTER channel beta
-$FLUTTER upgrade
-$FLUTTER pub version
 $FLUTTER config --enable-web
+$FLUTTER pub version
 $FLUTTER doctor
 $FLUTTER build web
