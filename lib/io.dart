b06d67f51f63065263cb0ac593ced0a0f51f9ae0
Carmine Zaccagnino <carmine@carminezacc.com>
2020-08-24 12:58:00 +0200

    prob fixed date error
    Signed-off-by: Carmine Zaccagnino <carmine@carminezacc.com>

--- lib/io.dart
+++ lib/io.dart
@@ -58,8 +58,7 @@ class LoginManager {
     Map resBody = json.decode(res.body);
 
     tokenStorage.writeJson("token", resBody["auth_token"]);
-    print("Roba = ${DateTime.fromMillisecondsSinceEpoch(resBody["refresh_token_expiry"]*1000).toIso8601String()}");
-    tokenStorage.writeJson("expiry", DateTime.fromMillisecondsSinceEpoch(resBody["refresh_token_expiry"]*1000).toIso8601String());
+    tokenStorage.writeJson("expiry", DateTime.fromMillisecondsSinceEpoch(int.parse(resBody["refresh_token_expiry"])*1000).toIso8601String());
 
     return resBody["success"];
   }
