2c8eaee1efb90fd6ba0f4318eecd9351a74a919b
Carmine Zaccagnino <carmine@carminezacc.com>
2020-08-25 11:40:33 +0200

    gittato utils.dart
    Signed-off-by: Carmine Zaccagnino <carmine@carminezacc.com>

---
+++ lib/utils.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart' show BuildContext, Navigator;
+import 'package:flutter/scheduler.dart' show SchedulerBinding;
+
+typedef ContextCallback = void Function(BuildContext);
+
+void goToRouteAsap(BuildContext context, String routeName) {
+  // waits for stuff to settle down before pushing
+  SchedulerBinding.instance.addPostFrameCallback(
+    (_) => Navigator.pushReplacementNamed(context, routeName)
+  );
+} 
+
+void doItAsap(BuildContext context, ContextCallback doStuff) {
+  // waits for stuff to settle down before doing stuff
+  SchedulerBinding.instance.addPostFrameCallback(
+    (_) => doStuff(context)
+  );
+} 
