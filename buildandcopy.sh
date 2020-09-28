515e483f7cb69cf5acdfb063329a25c1493bdb14
Carmine Zaccagnino <carmine@carminezacc.com>
2020-09-28 17:38:06 +0200

    sistemato script build and copy

--- buildandcopy.sh
+++ buildandcopy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 flutter build web --release --dart-define=FLUTTER_WEB_USE_SKIA=true
-scp -r -i ~/appunti.key build/web inginfo@appunti.me:/home/inginfo/flutter-appunti-web/build/
+scp -r -i $KEY_LOCATION build/web $USERNAME@appunti.me:/home/inginfo/flutter-appunti-web/build/
